80c16779fcdb048340239b5585fdf46605de2fff
mayur rajendra patil <mayurrajendrapatil@mayurs-MacBook-Pro.local>
2023-03-30 17:00:03 +0530

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom';
+import {BsCart3} from 'react-icons/bs';
+import Style from '../styles/navbar.module.css';
+import { CartContext } from '../cartContext/CardContext';
+
+function NavBar() {
+  const {product} = useContext(CartContext);
+  return (
+    <div className={Style.mainNavDiv}>
+        <div>
+            <h3>TeeRex Store</h3>
+        </div>
+        <div className={Style.cartMainDiv}>
+            <Link to="/"><h3>Products</h3></Link>
+            <div className={Style.cart}>
+            <Link to="/cart"><BsCart3 style={{width:'40px', height:'20px'}}/></Link>
+            <h4>{product.length}</h4>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
